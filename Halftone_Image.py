0e37aa4b27f5c418ac2a858391f839793b38562d
Vedanshi Raiyani <117573771+vedanshiraiyani@users.noreply.github.com>
2024-01-22 02:21:56 +0530

    Update Halftone_Image.py

--- Halftone_Image.py
+++ Halftone_Image.py
@@ -1,6 +1,9 @@
 from PIL import Image, ImageDraw
 import math
 
+
+
+# Determining the vertices of stars based on the centre and star size along with the sharpness of star (size in the below code is used to determing the ratio of distance of centre to outer point and distance of centre to inner point)
 def vertices_star(center_x, center_y, size, star_size):
     vertices_star = []
 
@@ -18,6 +21,7 @@ def vertices_star(center_x, center_y, size, star_size):
 
 
 
+# Determing the vertices of hexagon based and given centre of hexagon
 def vertices(x, y):
     vertices_hex = []
     num_sides = 6
@@ -32,9 +36,14 @@ def vertices(x, y):
     return vertices_hex
 
 
+
+
+# opening the image
 input_image = Image.open(r"C:\Users\hp\Desktop\nb.JPG").convert("L")
 new_image = input_image.resize((input_image.size[0]*10, input_image.size[1]*10)) # resizing should be done based on the size of input image
-print(new_image.size)
+# print(new_image.size)
+
+# determing the thresholds of brightness and star sizes(used to map with stars and hexagons)
 thresholds = [0, 30, 45, 70, 90, 120, 145, 160, 190, 220, 251]
 output_image = Image.new("L", new_image.size, "black")
 draw = ImageDraw.Draw(output_image)
@@ -42,6 +51,9 @@ size = [4.7, 4.4, 4.1, 3.8, 3.5, 3.2, 2.9, 2.6, 2.3, 2.0]
 star_size = 21
 polygon_count = 0
 dot_size = 40
+
+
+# iterating through the canvas with steps of fixed number of pixels as step size and drawing stars or hexagons
 
 for x in range(0, new_image.width, dot_size):
     for y in range(0, new_image.height, dot_size):
@@ -65,5 +77,7 @@ for x in range(0, new_image.width, dot_size):
                 draw.polygon(points, fill="white", outline = "black")
                 polygon_count += 1
 
+
+
 output_image.save("AG.jpg")
-print(polygon_count)
+print(polygon_count) #printing the total polygon count in the entire image
